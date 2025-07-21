a9e157299c075dc16bbabf012c4021602de27afd
조중석 <jungseok@jojungseog-ui-MacBookPro.local>
2025-07-21 12:03:26 +0900

    python update

--- PYTHON/카드 뭉치.py
+++ PYTHON/카드 뭉치.py
@@ -33,3 +33,15 @@ def solution(cards1, cards2, goal):
         else:
             return 'No'
     return answer
+
+
+## 코드 개선
+def solution(cards1, cards2, goal):    
+    for i in range(len(goal)):
+        if len(cards1) > 0 and goal[i] == cards1[0] :
+                cards1.pop(0)
+        elif len(cards2) > 0 and goal[i] == cards2[0] :
+                cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
