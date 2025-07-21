3411051994eff69f5f5f1849095f0bddc044887e
조중석 <jungseok@jojungseog-ui-MacBookPro.local>
2025-07-21 10:40:40 +0900

    file change

--- 3월에 태어난 여성 회원목록 출력하기.sql
+++ 3월에 태어난 여성 회원목록 출력하기.sql
@@ -1,12 +1,12 @@
-### 2025. 07. 21
+-- 2025. 07. 21
 
-#### SOL_1
+-- SOL_1
 SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
 from MEMBER_PROFILE
 where GENDER = 'W' and DATE_OF_BIRTH like '%-03-%' and TLNO is not null
 order by MEMBER_ID;
 
-#### SOL_2
+-- SOL_2
 SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH from member_profile
 where GENDER = 'W' and TLNO is not null and date_format(DATE_OF_BIRTH, '%m') = 3
 order by MEMBER_ID asc;
