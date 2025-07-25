586985b4e9a6d7a88549f8a5ea07c3d766bd27f2
seongjin <seongjin5743@naver.com>
2025-07-25 10:40:30 +0900

    성진

--- SQL/특정 물고기를 잡은 총 수 구하기.sql
+++ SQL/특정 물고기를 잡은 총 수 구하기.sql
@@ -1,21 +1,8 @@
 -- 2025. 07. 25
 
 -- SOL_1
-SELECT CAR_ID, round(avg(date(END_DATE) - date(start_date)), 1) as AVERAGE_DURATION from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-group by CAR_ID
-having AVERAGE_DURATION >= 7
-order by AVERAGE_DURATION desc, CAR_ID desc
+select count(a.fish_type) as FISH_COUNT from FISH_INFO a
+join FISH_NAME_INFO b on a.fish_type = b.fish_type
+where b.fish_name = 'BASS' or b.fish_name = 'SNAPPER';
 
 -- SOL_2
-select CAR_ID , round(avg(DATEDIFF(end_date, start_date)),1) as AVERAGE_DURATION
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-group by CAR_ID
-having avg(DATEDIFF(end_date, start_date)) >= 7
-order by AVERAGE_DURATION desc, car_id desc;
-
--- ANSWER(datediff에 +1을 해줘야함)
-select CAR_ID , round(avg(DATEDIFF(end_date, start_date)+1),1) as AVERAGE_DURATION
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY
-group by CAR_ID
-having AVERAGE_DURATION >= 7
-order by AVERAGE_DURATION desc, car_id desc;
