d05135571f96179428676bfb9941ca68fa09a57c
seongjin <seongjin5743@naver.com>
2025-07-28 10:22:19 +0900

    성진

--- PYTHON/예상 대진표.py
+++ PYTHON/예상 대진표.py
@@ -1,5 +1,12 @@
 # 2025. 07. 28
 
 # PYTHON_1
+def solution(n, a, b):
+    count = 1
+    while a % 2 != 1 and b - a != 1:
+        a = round(a / 2)
+        b = round(b / 2)
+        count += 1
+    return count
 
 # PYTHON_2
