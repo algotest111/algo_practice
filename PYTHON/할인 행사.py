72a6a5ff5f1384d7e0f95293e8210772ae6ee278
seongjin <seongjin5743@naver.com>
2025-07-29 10:25:37 +0900

    성진

--- PYTHON/할인 행사.py
+++ PYTHON/할인 행사.py
@@ -1,6 +1,49 @@
 # 2025. 07. 29
 
 # PYTHON_1
+def solution(want, number, discount):
+    answer = 0
+    fruits = []
+    
+    for w, n in zip(want, number):
+        fruits.extend([w] * n)
+    
+    fruits.sort()
+    
+    for i in range(len(discount) - 9):
+        qwer = discount[i:i+10]
+        qwer.sort()
+        if fruits == qwer:
+            answer += 1
+    return answer
 
+def solution(want, number, discount):
+    for i in range(len(discount) - 9):
+        flag = False
+        qwer = discount[i:i+10]
+        for j in range(len(want)):
+            if qwer.count(want[j]) >= number[j]:
+                flag = True
+            else:
+                flag = False
+                break
+        if flag:
+            answer += 1
 
+    return answer
+
+from collections import Counter
+
+def solution(want, number, discount):
+    answer = 0
+    fruits = []
+    
+    for w, n in zip(want, number):
+        fruits.extend([w] * n)
+        
+    for i in range(len(discount) - 9):
+        qwer = discount[i:i+10]
+        if Counter(fruits) == Counter(qwer):
+            answer += 1
+    return answer
 # PYTHON_2
