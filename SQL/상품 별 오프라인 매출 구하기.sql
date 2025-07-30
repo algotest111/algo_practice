8a01016651c0299d3143f131fbdeec505f8ab0fb
seongjin <seongjin5743@naver.com>
2025-07-30 10:10:12 +0900

    성진

--- SQL/상품 별 오프라인 매출 구하기.sql
+++ SQL/상품 별 오프라인 매출 구하기.sql
@@ -1,6 +1,9 @@
 -- 2025. 07. 30
 
 -- SOL_1
-
+SELECT a.PRODUCT_CODE, (sum(b.SALES_AMOUNT) * a.PRICE) as SALES from PRODUCT a
+join OFFLINE_SALE b on a.PRODUCT_ID = b.PRODUCT_ID
+group by a.PRODUCT_CODE
+order by SALES desc, a.PRODUCT_CODE
 
 -- SOL_2
